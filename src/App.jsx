a75b5f1fbedac454a86caaebae61753bced1b42d
boscocj16 <boscojoseph79@gmail.com>
2025-02-10 01:00:34 +0530

    Modified Sign In page UI (Log in and Sign up added)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from "./Components/Navbar";
 import Home from "./pages/Homepage";
 import Sellitems from "./pages/Sellitems";
 import Signin from "./pages/Signin";
+import Signup from "./Components/Signup";
+import Login from "./Components/Login";
+
 
 const App = () => {
   const location = useLocation();
@@ -15,6 +18,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/Sellitems" element={<Sellitems />} />
         <Route path="/Signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
     </>
   );
